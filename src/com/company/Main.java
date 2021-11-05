de040415b8df3bcf86b53732cbf5fa46b8791fca
DrRedyx <88320874+DrRedyx@users.noreply.github.com>
2021-11-05 22:11:28 +0300

    Revert "All 9 homework"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,29 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Author author1 = new Author("Stephen", "King");
-        Book book1 = new Book(author1, "IT", 1989);
-        System.out.println(book1.author.getName() + " " + book1.author.getSurName() + " " + book1.getPublishingYear());
-        book1.setPublishingYear(1950);
-        System.out.println(book1.getPublishingYear());
-        Author author2 = new Author("Andzhey", "Sapkovskiy");
-        Book book2 = new Book(author2, "Last wish", 1993);
-        Author author3 = new Author("Stan", "Lee");
-        Book book3 = new Book(author3, "Spider-Man", 1963);
-        Author author4 = new Author("Arthur", "Konan Doyle");
-        Book book4 = new Book(author4, "Sherlock Holmes", 1880);
-
-
-        Library library = new Library(4);
-        library.allOfBooks(book1);
-        library.allOfBooks(book2);
-        library.allOfBooks(book3);
-        library.allOfBooks(book4);
-        library.printBooks();
-        library.information(book1.getBookName());
-        library.newPublishingYear(book4.getBookName(), 1998);
-        library.information(book4.getBookName());
-
+	// write your code here
 
     }
 }
